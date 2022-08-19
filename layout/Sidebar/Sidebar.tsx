5eb60b03209a2e7039e447d5c73ca093eef950a9
Dima kun <FavoritePonyville@gmail.com>
2022-08-19 13:35:09 +0400

    fix

--- layout/Sidebar/Sidebar.tsx
+++ layout/Sidebar/Sidebar.tsx
@@ -2,14 +2,11 @@ import React from 'react';
 import { SidebarProps } from './Sidebar.props';
 import s from './Sidebar.module.css';
 
-const {sidebar} = s;
+const {sidebar,menu} = s;
 const Sidebar = ({ className, ...props}:SidebarProps):JSX.Element => {
     return (
         <div className={sidebar} {...props}>
-            <div> sidebar</div>
-            <div> sidebar</div>
-            <div> sidebar</div>
-            <div> sidebar</div>
+            <div className={menu}> <div>mfpdsfmsds</div></div>
 
         </div>
     );
