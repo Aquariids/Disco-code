e138fc09f7a7d523eaf494507c2696fa452fe97d
Dima kun <FavoritePonyville@gmail.com>
2022-08-24 23:19:58 +0400

    fix

--- layout/Sidebar/Sidebar.props.ts
+++ layout/Sidebar/Sidebar.props.ts
@@ -1,4 +1,4 @@
-import { PostMeta } from "@/src/api";
+import { PostMeta } from "../../src/api";
 import { DetailedHTMLProps, HTMLAttributes } from "react";
 
 export interface SidebarProps extends  DetailedHTMLProps<HTMLAttributes<HTMLDivElement>, HTMLDivElement> {
