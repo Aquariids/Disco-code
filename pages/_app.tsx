64d3444b7e99e08b688e52a20e56e37e224f1753
Dima kun <FavoritePonyville@gmail.com>
2022-08-24 23:14:56 +0400

    getStaticProps, getStaticPaths

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,12 @@
 import '../styles/globals.css';
+import "@code-hike/mdx/dist/index.css"
 import type { AppProps } from 'next/app';
-import Head from 'next/head';
-
 
 function MyApp({ Component, pageProps }: AppProps): JSX.Element {
-
   return <Component {...pageProps} />;
+  
 }
 
 export default MyApp;
+
+
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,38 @@
-import type { NextPage } from 'next';
+import type { GetStaticProps, NextPage } from 'next';
+import {getAllPosts, PostMeta} from '../src/api';
 import { withLayout } from '../layout/Layout';
+import React from 'react';
+import IPost from '../interface/IPost';
+import Articles from '../src/Components/Articles';
 
-const Home: NextPage = (): JSX.Element => {
+export interface IProps extends Record<string,unknown> {
+  posts:PostMeta[]
+}
+
+const Home: NextPage<IProps> = ({ posts }: {posts:PostMeta[]}): JSX.Element => {
+
   return (
-    <></>
+    <div>
+   <Articles posts ={posts}/>
+    </div>
   );
 };
 
+
+
+
+
 export default withLayout(Home);
+
+export const getStaticProps: GetStaticProps = async () => {
+
+  const posts = getAllPosts()
+  .slice(0,9)
+  .map(post => post.meta);
+
+  return {
+    props: {
+      posts
+    }
+  };
+};
