a9c2519faf63fafbbdfbff8b199207e983f161d4
Dima kun <FavoritePonyville@gmail.com>
2022-09-03 03:30:29 +0400

    test test

--- src/Components/SidebarMenu/SidebarMenu.tsx
+++ src/Components/SidebarMenu/SidebarMenu.tsx
@@ -1,33 +1,37 @@
 import Link from 'next/link';
-import React, { useContext, useState } from 'react';
+import React, { useContext, useEffect, useRef, useState } from 'react';
 import { AppContext } from '../../../context/app.context';
 import { useRouter } from "next/router";
 import s from './SidebarMenu.module.css';
 import { PostMeta } from '../../../pages/api/api';
 import cn from 'classnames';
 
-
 const SidebarMenu = (): JSX.Element => {
-    const [dropdownState, setDropdownState] = useState({ open: false });
-    const handleDropdownClick = () => {
-        setDropdownState({ open: !dropdownState.open }); 
-    };
-
+   
     const router = useRouter();
     const { posts } = useContext(AppContext);
     const pathname = router.asPath;
 
+
     const buildMenuJs = (menu: (post: PostMeta, category: string) => JSX.Element): JSX.Element => {
-    
-        return (
-            <nav className={s.navMenu}>
-                <div>
-                    <button
+
+        const buildBasicJs = () => {
+            const [isOpen, setOpen] = useState(false);
+            const handleToggle = () => {
+                setOpen(!isOpen);
+                
+            };
+            
+            return (
+                <>
+                <button
                         type="button"
-                        className={s.button}
-                        onClick={handleDropdownClick}
+                        className={cn(s.button, {
+                            [s.activeBtn]: isOpen === true,
+                        })}
+                        onClick={handleToggle}
                     >Базовый javaScript</button>
-                    {dropdownState.open && (
+                    {isOpen && (
                         <div className={s.dropdown}>
                             <ul className={s.listMenu}>
                         {posts && posts.map((post, index) => (
@@ -38,23 +42,65 @@ const SidebarMenu = (): JSX.Element => {
                     </ul>
                         </div>
                     )}
+                </>
+            );
+        };
+
+
+
+        const buildAdvancedJs = () => { 
+            const myRef = useRef();
+
+            useEffect(()=>{
+                console.log(myRef);
                 
-                </div>
-                <div>
-                    <button>Продвинутый javaScript</button>
-                    <ul className={s.listMenu}>
+              });
+            const [isOpen, setOpen] = useState(false);
+            const handleToggle = () => {
+                setOpen(!isOpen);
+                
+            };
+
+            return(
+                <>
+                <button
+                        type="button"
+                        className={cn(s.button,{
+                            [s.activeBtn]: isOpen === true,
+
+                        })}
+                        onClick={handleToggle}
+                    >Продвинутый javaScript</button>
+                    {isOpen && (
+                        <div
+                        className={s.dropdown}>
+                            <ul className={s.listMenu}>
                         {posts && posts.map((post, index) => (
                             <React.Fragment key={index}>
                                 {menu(post, 'none')}
                             </React.Fragment>
                         ))}
                     </ul>
+                        </div>
+                    )}
+                </>
+            )
+
+        };
+        return (
+            <nav className={s.navMenu}>
+                <div>
+                    {buildBasicJs()}
+                </div>
+                <div>
+                    {buildAdvancedJs()}
                 </div>
             </nav>
         );
     };
 
     const buildMenuGrid = (menu: (post: PostMeta, category: string) => JSX.Element): JSX.Element => {
+    
         return (
             <div className={s.navMenu}>
                 {posts && posts.map((post, index) => (
@@ -70,14 +116,14 @@ const SidebarMenu = (): JSX.Element => {
         if (pathname.length > 5) {
             
             return (
-                <li
+                <li 
                     className={cn({
                         [s.active]: pathname.replace(post.slug, '') + post.slug == pathname,
                         [s.link]: pathname.replace(post.slug, '') + post.slug !== pathname,
                         [s.show]: post.category == category,
                         [s.hide]: post.category !== category,
                     })} >
-                    <Link className={cn('lox')} href={`${post.slug}`}>{post.title}</Link>
+                    <Link href={`${post.slug}`}>{post.title}</Link>
                 </li>
 
             );
@@ -88,7 +134,6 @@ const SidebarMenu = (): JSX.Element => {
         }
 
     };
-
 
     if (router.pathname === '/js/[js]') {
         return (
