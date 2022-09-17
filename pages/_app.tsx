06ac030291462d00d1eb8a37a4df7980b19c807d
Dima kun <FavoritePonyville@gmail.com>
2022-09-17 10:10:10 +0400

    fix loader

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -11,7 +11,7 @@ export function Loading():any {
 
   useEffect(() => {
     const handleStart = (url:string) => (url !== router.asPath) && setTimeout(() => {setLoading(true);},1000);
-    const handleComplete = (url:string) => (url === router.asPath) && setTimeout(() => {setLoading(false);});
+    const handleComplete = (url:string) => (url === router.asPath) && setLoading(false);
     router.events.on('routeChangeStart',handleStart);
     router.events.on('routeChangeComplete',handleComplete);
     router.events.on('routeChangeError',handleComplete);
