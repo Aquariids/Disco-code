3942cd49e896ff540da380a13f7fc245bd55b1ce
Dima kun <FavoritePonyville@gmail.com>
2022-09-17 10:34:10 +0400

    fix loader

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,6 +4,7 @@ import type { AppProps } from 'next/app';
 import { useRouter } from 'next/router';
 import Head from 'next/head';
 import { useEffect, useState } from 'react';
+import cn from 'classnames';
 
 export function Loading():any {
   const router = useRouter();
@@ -26,7 +27,9 @@ export function Loading():any {
   });
 
   return loading && (
-<span className="loader">
+<span className={cn('loader', {
+  ['endLoader']: loading == false
+})}>
 </span>
   );
 }
