5c9f6ff3db6fd371db52351c94915cc6fb7b23bb
Dima kun <FavoritePonyville@gmail.com>
2022-09-17 13:47:04 +0400

    fixxxxxxxxxxx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,8 +3,35 @@ import "@code-hike/mdx/dist/index.css";
 import type { AppProps } from 'next/app';
 import { useRouter } from 'next/router';
 import Head from 'next/head';
+import { useEffect, useState } from 'react';
+
+export function Loading():any {
+  const router = useRouter();
+  const [loading,setLoading] = useState(false);
+ 
+  useEffect(() => {
+      const handleStart = (url:string) => (url !== router.asPath) && setTimeout(()=> {setLoading(true);},1000);
+      const handleComplete = (url:string) => (url === router.asPath || router.asPath.startsWith('/js/basicjs')) && setLoading(false);
 
 
+      router.events.on('routeChangeStart',handleStart);
+      router.events.on('routeChangeComplete',handleComplete);
+      router.events.on('routeChangeError',handleComplete);  
+    
+    return () => {
+      router.events.off('routeChangeStart',handleStart);
+      router.events.off('routeChangeComplete',handleComplete);
+      router.events.off('routeChangeError',handleComplete);
+  
+    };
+  
+  });
+
+  return loading && (
+<span className = {'loader'} >
+</span>
+  );
+}
 function MyApp({ Component, pageProps }: AppProps): JSX.Element {
 const router = useRouter();
   return(
