044d694ad85d225ee9e65ec8f2c78182d8c12036
Dima kun <FavoritePonyville@gmail.com>
2022-09-17 12:30:22 +0400

    fix

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -13,19 +13,19 @@ export function Loading({mdx}:Iload):any {
   const [loading,setLoading] = useState(false);
 
   useEffect(() => {
-    const handleStart = (url:string) => (url !== router.route) && setTimeout( () => {setLoading(true);},500);
+    const handleStart = () => setTimeout( () => {setLoading(true);},500);
     const handleComplete = () => setLoading(false);
     router.events.on('routeChangeStart',handleStart);
-    router.events.on('routeChangeComplete',handleComplete);
+    router.events.on("routeChangeComplete",handleComplete);
     router.events.on('routeChangeError',handleComplete);
 
 
-    return () => {
-      router.events.off('routeChangeStart',handleStart);
-      router.events.off('routeChangeComplete',handleComplete);
-      router.events.off('routeChangeError',handleComplete);
+    // return () => {
+    //   router.events.off('routeChangeStart',handleStart);
+    //   router.events.off('routeChangeComplete',handleComplete);
+    //   router.events.off('routeChangeError',handleComplete);
   
-    };
+    // };
   });
 
   return loading && (
