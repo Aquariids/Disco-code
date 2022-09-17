763a11161e97a31d00e2330d9d818649107205ce
Dima kun <FavoritePonyville@gmail.com>
2022-09-17 15:28:36 +0400

    sslfknsdjkf

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,42 +4,28 @@ import type { AppProps } from 'next/app';
 import { useRouter } from 'next/router';
 import Head from 'next/head';
 import { useEffect, useState } from 'react';
-
 import NProgress from 'nprogress';
 export function  Loading():any  {
+  const [load,setLoad] = useState(false);
+
   const router = useRouter();
-  const [loading,setLoading] = useState(false);
+  const [loading,setLoading] = useState(load);
  
   useEffect(() => {
       // const handleStart =  (url:string) =>   (url !== router.asPath) && setLoading(true);
       // const handleComplete =  (url:string) =>  (url === router.asPath) && setLoading(false);
       router.events.on('routeChangeStart',(url) => {
-        NProgress.start();
-
         setTimeout(()=> {
+          NProgress.start();
           setLoading(true);
 
-        },1000);
-
-
+        },1000)
 
       });
       router.events.on('routeChangeComplete',(url) => {
         NProgress.done();
         setLoading(false);
       });
-    
-    return () => {
-      router.events.off('routeChangeStart',(url) => {
-        NProgress.start();
-
-          setLoading(true);
-
-      });
-      router.events.off('routeChangeComplete',(url) => {
-        setLoading(false);
-      });
-    };
   
   });
 
