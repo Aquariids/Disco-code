53873a0f0be6966bbedb7ba22b63511ead2bd772
Dima kun <FavoritePonyville@gmail.com>
2022-09-17 13:31:20 +0400

    fix

--- layout/Header/Header.tsx
+++ layout/Header/Header.tsx
@@ -2,7 +2,6 @@ import React from 'react';
 import HeaderMenu from '../../src/Components/HeaderMenu/HeaderMenu';
 import { HeaderProps } from './Header.props';
 import s from './Header.module.css';
-import { Loading } from '../../pages/_app';
 
 const Header = ({ ...props }:HeaderProps): JSX.Element => {
     return (
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,7 +3,6 @@ import "@code-hike/mdx/dist/index.css";
 import type { AppProps } from 'next/app';
 import { useRouter } from 'next/router';
 import Head from 'next/head';
-import { useEffect, useState } from 'react';
 
 
 function MyApp({ Component, pageProps }: AppProps): JSX.Element {
