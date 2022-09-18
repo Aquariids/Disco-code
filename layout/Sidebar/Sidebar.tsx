1a41ef4911c809957dfabf3510fc6088b53dfca5
Dima kun <FavoritePonyville@gmail.com>
2022-09-18 17:25:58 +0400

    fix

--- layout/Sidebar/Sidebar.tsx
+++ layout/Sidebar/Sidebar.tsx
@@ -3,15 +3,14 @@ import { SidebarProps } from './Sidebar.props';
 import s from './Sidebar.module.css';
 import SidebarMenu from '../../src/Components/SidebarMenu/SidebarMenu';
 import { useRouter } from 'next/router';
-import cn from 'classnames';
 
 const {sidebar} = s;
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 const Sidebar = ({className, ...props}:SidebarProps):JSX.Element => {
     const router = useRouter(); 
-    const asPath = router.asPath;
-    
-    if(asPath.length < 18){
+    const r = router.pathname;
+
+    if(r === '/js' || r === '/' || r === '/ts' || r === '/react' || r === '/next' ){
         return(
             <></>
         );
