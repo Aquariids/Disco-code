c0ea369c7b374ead19b4073e84e50506c40df534
Dima-kun <FaboritePonyville@gmail>
2022-09-20 20:05:46 +0400

    fix

--- pages/react/basicreact/[react].tsx
+++ pages/react/basicreact/[react].tsx
@@ -49,7 +49,7 @@ const router = useRouter();
                     <MDXRemote  {...post.source} />
 	
 
-        <div className={cn(s.edit,'page_edit')}><a target='_blank' href={`https://github.com/Aquariids/Disco-code/blob/main/Content/react/${post.meta.slug}.mdx`}> Отредактировать эту страницу</a><img style={{width:'30px',paddingLeft:'5px'}} src='/edit.svg'/></div>
+        <div className={cn(s.edit,'page_edit')}><a target='_blank' rel ='noopener' href={`https://github.com/Aquariids/Disco-code/blob/main/Content/react/${post.meta.slug}.mdx`}> Отредактировать эту страницу</a><img style={{width:'30px',paddingLeft:'5px'}} src='/edit.svg'/></div>
         <div className={cn(s.footer,'page_footer')}>
             <Link href={post.meta.prev}>{post.meta.prev === 'none'?<span></span>:'Предыдущая страница'}</Link>
             <Link href={post.meta.next}>{post.meta.next === 'none'?'':'Следующая страница'}</Link>
--- pages/ts/basicts/[ts].tsx
+++ pages/ts/basicts/[ts].tsx
@@ -45,7 +45,7 @@ const PostPage: NextPage<never> = ({ post }: { post: MSXPost }): JSX.Element => 
                 >
                     <h1>{post.meta.title}</h1>
                     <MDXRemote  {...post.source} />
-        <div className={cn(s.edit, 'page_edit')}><a target='_blank' href={`https://github.com/Aquariids/Disco-code/blob/main/Content/typescript/${post.meta.slug}.mdx`}> Отредактировать эту страницу</a><img style={{width:'30px',paddingLeft:'5px'}} src='/edit.svg'/></div>
+        <div className={cn(s.edit, 'page_edit')}><a target='_blank' rel ='noopener' href={`https://github.com/Aquariids/Disco-code/blob/main/Content/typescript/${post.meta.slug}.mdx`}> Отредактировать эту страницу</a><img style={{width:'30px',paddingLeft:'5px'}} src='/edit.svg'/></div>
         <div className={cn(s.footer, 'page_footer')}>
             <Link href={post.meta.prev}>{post.meta.prev === 'none'?<span></span>:'Предыдущая страница'}</Link>
             <Link href={post.meta.next}>{post.meta.next === 'none'?<span></span>:'Следующая страница'}</Link>
