7d397bbc437a91b543030aa3f1a6ee266ca0714c
Dima-kun <FaboritePonyville@gmail>
2022-09-22 18:20:58 +0400

    fix

--- pages/js/index.tsx
+++ pages/js/index.tsx
@@ -48,8 +48,8 @@ const Home: NextPage = (): JSX.Element => {
 
           <Menu title="Базовый JavaScript" category='basicjs' page='js' />
           <Menu title="Продвинутый JavaScript" category='advancedjs' page='js' />
-          <Menu title="Алгоритмы" category='advancedjs' page='js' />
-          <Menu title="Простенькие задачи" category='advancedjs' page='js' />
+          <Menu title="Алгоритмы" category='.' page='js' />
+          <Menu title="Простенькие задачи" category='.' page='js' />
 
         </div>
       </div>
