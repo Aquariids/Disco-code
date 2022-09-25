05c40641591d02194d83760205438fb1b2f9d472
Dima kun <FavoritePonyville@gmail.com>
2022-09-25 14:07:14 +0400

    fix test

--- pages/index.tsx
+++ pages/index.tsx
@@ -15,7 +15,7 @@ const Home: NextPage = (): JSX.Element => {
         <title>DiscoCode - уроки по javascript, typescript, react, nextjs</title>
         <meta name="google-site-verification" content="ArMplWlyr69JYGz_vTfAjA8HzzYLdXm-p5gHjqgDihY" />
         <meta name="yandex-verification" content="a99ae512e4f1c330" />
-        <meta name='description' content={'Уроки по typescript,ts,уроки по javaScript,js, уроки по react,reactjs,next,next.js,nextjs статьи,'}/>
+        <meta name='description' content={'Здесь вы можете найти разборы разных тем на  javaScript, typeScript, reactjs, nextjs'}/>
         <meta property='og:title' content='Уроки и разборы разных тем по  javascript, typescript, react, nextjs' />
         <meta property='og:description' content={'Уроки и разбор разных тем по javascript, typescript, react, nextjs'} />
         <meta property='og:type' content={'article'} />
