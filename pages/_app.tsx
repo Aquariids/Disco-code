7d5b315d1d11f5008f6a0b126fd20e505de3d9ce
Dima kun <FavoritePonyville@gmail.com>
2022-09-30 02:26:13 +0400

    да что же такое

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,30 @@
 import '../styles/globals.css';
 import type { AppProps } from 'next/app';
 import Head from 'next/head';
-import { useRouter } from 'next/router';
+import ym from 'react-yandex-metrika';
+import {YMInitializer} from 'react-yandex-metrika';
+import Router, { useRouter } from 'next/router';
+import { Loading } from '../src/Components/Loading/Loading';
+Router.events.on('routeChangeComplete', (url:string)=> {
+  if(typeof window !== 'undefined')
+  ym('hit',url);
+});
 
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+export async function getServerSideProps({ req, res }:any) {
+  res.setHeader(
+    'Cache-Control',
+    'public, s-maxage=10, stale-while-revalidate=59'
+  );
 
+  return {
+    props: {},
+  };
+}
 
+// export function reportWebVitals(metric: any) {
+//   console.log(metric);
+// }
 
 function MyApp({ Component, pageProps }: AppProps): JSX.Element {
   const router = useRouter();
@@ -15,10 +35,16 @@ function MyApp({ Component, pageProps }: AppProps): JSX.Element {
       <Head>
       <meta name="yandex-verification" content="a99ae512e4f1c330" />
         <meta name="google-site-verification" content="ArMplWlyr69JYGz_vTfAjA8HzzYLdXm-p5gHjqgDihY" />
+        <link rel='preconnect' href='https://mc.yandex.ru'/>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <meta property='og:url' content={`discocode.ru${router.asPath}`}/>
         <meta property='og:locale' content={'ru_RU'}/>
       </Head>
+      <YMInitializer
+      accounts={[]}
+      options={{webvisor:false,defer:true}}
+      version = "2"
+      />
       <Component {...pageProps} key={router.pathname} /></>
   );
 }
