4202e5f33d5e67c6887eb95889287b82a7d135f9
Dima kun <FavoritePonyville@gmail.com>
2022-09-30 02:04:58 +0400

    fix

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,7 @@
 import '../styles/globals.css';
 import type { AppProps } from 'next/app';
 import Head from 'next/head';
-import ym from 'react-yandex-metrika';
-import Router, { useRouter } from 'next/router';
+import { useRouter } from 'next/router';
 
 
 
@@ -16,7 +15,6 @@ function MyApp({ Component, pageProps }: AppProps): JSX.Element {
       <Head>
       <meta name="yandex-verification" content="a99ae512e4f1c330" />
         <meta name="google-site-verification" content="ArMplWlyr69JYGz_vTfAjA8HzzYLdXm-p5gHjqgDihY" />
-        <link rel='preconnect' href='https://mc.yandex.ru'/>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <meta property='og:url' content={`discocode.ru${router.asPath}`}/>
         <meta property='og:locale' content={'ru_RU'}/>
