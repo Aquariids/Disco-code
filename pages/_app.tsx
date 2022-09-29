56d73239b0c8cace28ba526924c715990cea8560
Dima kun <FavoritePonyville@gmail.com>
2022-09-30 02:00:05 +0400

    fix

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,13 +2,8 @@ import '../styles/globals.css';
 import type { AppProps } from 'next/app';
 import Head from 'next/head';
 import ym from 'react-yandex-metrika';
-import {YMInitializer} from 'react-yandex-metrika';
 import Router, { useRouter } from 'next/router';
-import { Loading } from '../src/Components/Loading/Loading';
-Router.events.on('routeChangeComplete', (url:string)=> {
-  if(typeof window !== 'undefined')
-  ym('hit',url);
-});
+
 
 
 
@@ -26,11 +21,6 @@ function MyApp({ Component, pageProps }: AppProps): JSX.Element {
         <meta property='og:url' content={`discocode.ru${router.asPath}`}/>
         <meta property='og:locale' content={'ru_RU'}/>
       </Head>
-      <YMInitializer
-      accounts={[]}
-      options={{webvisor:true,defer:true}}
-      version = "2"
-      />
       <Component {...pageProps} key={router.pathname} /></>
   );
 }
