7317a37c8e583af86ddd74c50f40c73ed672fd05
Dima kun <FavoritePonyville@gmail.com>
2022-10-01 18:12:18 +0400

    add color js,ts,react  for mobileBtn depending on the open page

--- src/Components/UI/MobileButton/MobileButton.props.ts
+++ src/Components/UI/MobileButton/MobileButton.props.ts
@@ -1,7 +1,6 @@
 import { ButtonHTMLAttributes, DetailedHTMLProps } from "react";
 
 export interface MobileButtonProps extends DetailedHTMLProps<ButtonHTMLAttributes<HTMLButtonElement>, HTMLButtonElement> {
-    path?:string;
     setMobile?:any;
     mobile?: boolean;
 }
--- src/Components/UI/MobileButton/MobileButton.tsx
+++ src/Components/UI/MobileButton/MobileButton.tsx
@@ -1,11 +1,13 @@
-import Link from 'next/link';
 import React from 'react';
 import s from './MobileButton.module.css';
 import { MobileButtonProps } from './MobileButton.props';
 import ArrowIcon from './arrow.svg';
 import cn from 'classnames';
+import { useRouter } from 'next/router';
 
-const MobileButton = ({ path, mobile, setMobile, ...props }: MobileButtonProps) => {
+const MobileButton = ({ mobile, setMobile, ...props }: MobileButtonProps) => {
+    const router = useRouter();
+    
     function mob() {
         setMobile(!mobile);
     }
@@ -15,10 +17,20 @@ const MobileButton = ({ path, mobile, setMobile, ...props }: MobileButtonProps) 
                 <div className={s.svg}>
                     <div className={cn(s.arrows)}>
                     <span className={cn(s.arrowTop, {
-                        [s.arrowTopTrue]: mobile === true
+                        [s.arrowTopTrue]: mobile === true,
+                        [s.arrowsJs]: router.pathname.startsWith('/js'),
+                        [s.arrowsReact]: router.pathname.startsWith('/react'),
+                        [s.arrowsTs]: router.pathname.startsWith('/ts'),
+
+
                     })}><ArrowIcon /></span>
                     <span className={cn(s.arrowDown, {
-                    [s.arrowDownTrue]: mobile === true
+                    [s.arrowDownTrue]: mobile === true,
+                    [s.arrowsJs]: router.pathname.startsWith('/js'),
+                    [s.arrowsReact]: router.pathname.startsWith('/react'),
+                    [s.arrowsTs]: router.pathname.startsWith('/ts'),
+
+
                     })}><ArrowIcon /></span>
                     </div>
                 </div>
