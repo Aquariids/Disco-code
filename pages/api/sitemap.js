4e0f3c4a050a8060d6f7f4dcbc511f4361a30816
Dima kun <FavoritePonyville@gmail.com>
2022-11-21 07:37:22 +0300

    fix

---
+++ pages/api/sitemap.js
@@ -0,0 +1,66 @@
+import { getAllPosts } from "./api";
+import { SitemapStream, streamToPromise } from 'sitemap';
+import { Readable } from 'stream';
+import { POSTS_PATH_JS, POSTS_PATH_NEXTJS, POSTS_PATH_PARADIGMS, POSTS_PATH_REACT, POSTS_PATH_TS } from "./paths";
+
+
+export default async (req,res) => {
+    const metaJs = getAllPosts(POSTS_PATH_JS).map((post) => post.meta);
+    const metaTs = getAllPosts(POSTS_PATH_TS).map((post) => post.meta);
+    const metaNextJs = getAllPosts(POSTS_PATH_NEXTJS).map((post) => post.meta);
+    const metaPr = getAllPosts(POSTS_PATH_PARADIGMS).map((post) => post.meta);
+    const metaReact = getAllPosts(POSTS_PATH_REACT).map((post) => post.meta);
+
+
+    const links = [];
+
+    
+    const stream = new SitemapStream({ hostname: `https://${req.headers.host}` });
+    metaJs.forEach((post)=> {
+        stream.write({
+            url:`/js/${post.category}/${post.slug}`,
+            changefreq:"daily",
+            priority:0.9
+        });
+    });
+    metaTs.forEach((post)=> {
+        stream.write({
+            url:`/ts/${post.category}/${post.slug}`,
+            changefreq:"daily",
+            priority:0.9
+        });
+    });
+    metaNextJs.forEach((post)=> {
+        stream.write({
+            url:`/next-js/${post.category}/${post.slug}`,
+            changefreq:"daily",
+            priority:0.9
+        });
+    });
+    metaReact.forEach((post)=> {
+        stream.write({
+            url:`/react/${post.category}/${post.slug}`,
+            changefreq:"daily",
+            priority:0.9
+        });
+    });
+    metaPr.forEach((post)=> {
+        stream.write({
+            url:`/paradigms/${post.category}/${post.slug}`,
+            changefreq:"daily",
+            priority:0.9
+        });
+    });
+
+    res.writeHead(200,{
+        "Content-Type":"application/xml"
+    });
+
+    const xmlString = await streamToPromise(
+        Readable.from(links).pipe(stream)
+    ).then((data)=> data.toString());
+
+    res.end(xmlString);
+    
+};
+
