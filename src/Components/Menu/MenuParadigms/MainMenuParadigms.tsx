5505eeb4f2ef713c7cd53b14c97fe3a60a92a3b3
Dima kun <FavoritePonyville@gmail.com>
2022-11-22 17:44:21 +0300

    fix

--- src/Components/Menu/MenuParadigms/MainMenuParadigms.tsx
+++ src/Components/Menu/MenuParadigms/MainMenuParadigms.tsx
@@ -2,12 +2,13 @@ import React, { useContext } from 'react';
 import { AppContext } from '../../../../context/app.context';
 import { PostMeta } from '../../../../pages/api/api';
 import Menu from '../Menu';
-import { ParadigmsContextPosts } from './postsParadigms.props';
+import { ParadigmsContextProps } from './postsParadigms.props';
 
 const MainMenuParadigms = (): JSX.Element => {
 
     const { AllThemePosts } = useContext(AppContext);
-    const {post_Style_Paradigms}:ParadigmsContextPosts<PostMeta[]> = AllThemePosts;
+    const {post_Style_Paradigms}:ParadigmsContextProps<PostMeta[]> = AllThemePosts;
+
         return (
             <>
                 <Menu posts={post_Style_Paradigms} title=" Стили написания" category='style' page='paradigms' />
