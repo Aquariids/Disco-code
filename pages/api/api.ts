547baecaa4e9dc74d9fcd858c4be40aef77c5c23
Dima kun <FavoritePonyville@gmail.com>
2022-11-29 17:58:02 +0300

    new pages aboud web response & request

--- pages/api/api.ts
+++ pages/api/api.ts
@@ -18,7 +18,6 @@ export const getSlugs = (url: string): string[] => {
 
 export const getAllPosts = (url: string) => {
   const posts = getSlugs(url).map((slug) => getPostFromSlug(slug, url));
-
   return posts;
 };
 
@@ -59,3 +58,4 @@ export const getPostFromSlug = (slug: string, url: string): Post => {
     },
   };
 };
+
