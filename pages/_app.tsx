9944a369b7bca6bafcf4c7037e3e1a7c20d0be7d
Dima kun <FavoritePonyville@gmail.com>
2022-12-07 16:00:19 +0300

    fix

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -17,6 +17,7 @@ const MyApp = ({ Component, pageProps }: AppProps): JSX.Element => {
   const r = router.asPath.split('/').length;
 
   function telegramChat () {
+    const urlChat = 'https://widget.replain.cc/dist/client.js?id=3c7a4665-2ba9-4f82-9bd2-d3f54e2bbb6b';
     const chat = document.querySelector('#__replain_widget') as HTMLElement;
     const headers = document.querySelectorAll('script');
     function loadScript(src:string) { 
@@ -26,12 +27,13 @@ const MyApp = ({ Component, pageProps }: AppProps): JSX.Element => {
       document.head.append(script); 
   }
   if(router.asPath === '/') {
-    loadScript('https://widget.replain.cc/dist/client.js?id=3c7a4665-2ba9-4f82-9bd2-d3f54e2bbb6b');
+    loadScript(urlChat);
     if(chat) chat.style.display = 'block';
   } else {    
     headers.forEach(item => {
-      if(item.src == 'https://widget.replain.cc/dist/client.js?id=3c7a4665-2ba9-4f82-9bd2-d3f54e2bbb6b') {
+      if(item.src == urlChat) {
         if(chat) chat.style.display = 'none';
+        item.src = '';
         item.remove();
       } 
       
