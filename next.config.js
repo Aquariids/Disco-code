c7b002461df4364647d6324e26cd419639871c64
Dima kun <FavoritePonyville@gmail.com>
2022-12-09 12:44:39 +0300

    instal pwa test

--- next.config.js
+++ next.config.js
@@ -1,4 +1,27 @@
+// eslint-disable-next-line @typescript-eslint/no-var-requires, no-undef
+const runtimeCaching = require('next-pwa/cache')
+
+// eslint-disable-next-line no-undef
 module.exports = {
+	withPWA(config) {
+		config.module.rules.push({
+			reactStrictMode: true,
+		i18n: {
+			locales: ['ru', 'nl'],
+			defaultLocale: 'ru',
+			localeDetection: false,
+		},
+		pwa: {
+			// eslint-disable-next-line no-undef
+			disable: process.env.NODE_ENV !== 'production',
+			dest: 'public',
+			runtimeCaching,
+			buildExcludes: [/middleware-manifest.json$/],
+		},
+		});
+
+		return config;
+	},
 	webpack(config) {
 		config.module.rules.push({
 			loader: '@svgr/webpack',
@@ -8,22 +31,25 @@ module.exports = {
 				svgo: true,
 				svgoConfig: {
 					plugins: [{
-						name:'preset-default',
+						name: 'preset-default',
 						params: {
 							override: {
-								removeViewBox:false
+								removeViewBox: false
 							}
 						}
 					}]
 				},
+
 				titleProp: true,
+
 			},
 			test: /\.svg$/,
-			
 		});
-		
+
 
 		return config;
 	},
+
+	
 };
 
