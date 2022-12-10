d8bfb90330854af379b7b989838cd94d3da72c0a
Dima kun <FavoritePonyville@gmail.com>
2022-12-10 17:34:22 +0300

    fix logo, add new fonts

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,12 +3,12 @@ import '../styles/globals.css';
 import type { AppProps } from 'next/app';
 import Head from 'next/head';
 import ym from 'react-yandex-metrika';
-import {YMInitializer} from 'react-yandex-metrika';
+import { YMInitializer } from 'react-yandex-metrika';
 import Router, { useRouter } from 'next/router';
 import { useEffect } from 'react';
-Router.events.on('routeChangeComplete', (url:string)=> {
-  if(typeof window !== 'undefined')
-  ym('hit',url);
+Router.events.on('routeChangeComplete', (url: string) => {
+  if (typeof window !== 'undefined')
+    ym('hit', url);
 });
 
 
@@ -20,7 +20,7 @@ const MyApp = ({ Component, pageProps }: AppProps): JSX.Element => {
   //   const urlChat = 'https://widget.replain.cc/dist/client.js?id=3c7a4665-2ba9-4f82-9bd2-d3f54e2bbb6b';
   //   const chat = document.querySelector('#__replain_widget') as HTMLElement;
   //   const chat2 = document.querySelector('#__replain_widget_iframe ') as HTMLElement;
-    
+
   //   const headers = document.querySelectorAll('script');
   //   function loadScript(src:string) { 
   //     const script = document.createElement('script'); 
@@ -48,7 +48,7 @@ const MyApp = ({ Component, pageProps }: AppProps): JSX.Element => {
   //       item.src = '';
   //       item.remove();
   //     } 
-      
+
   //   }); 
   // }
   // }
@@ -56,33 +56,36 @@ const MyApp = ({ Component, pageProps }: AppProps): JSX.Element => {
   function smothScroll() {
     const html = document.querySelector('html') as HTMLElement;
 
-    if(r >= 3) { // при переходе к темам с главной страници, мне не нужен плавный скролл
-      html.style.scrollBehavior = 'smooth'; 
+    if (r >= 3) { // при переходе к темам с главной страници, мне не нужен плавный скролл
+      html.style.scrollBehavior = 'smooth';
     } else {
-      html.style.scrollBehavior = 'auto'; 
+      html.style.scrollBehavior = 'auto';
     }
   }
-  useEffect(()=> {
+  useEffect(() => {
     // telegramChat();
     smothScroll();
-  },[router]);
-  
+  }, [router]);
+
   return (
     <>
       <Head>
-      <meta name="viewport" content="viewport-fit=cover" />
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" />
+        <link href="https://fonts.googleapis.com/css2?family=Noto+Sans:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;1,100&display=swap" rel="stylesheet" />
+        <meta name="viewport" content="viewport-fit=cover" />
         <meta name="yandex-verification" content="a99ae512e4f1c330" />
         <meta name="google-site-verification" content="ArMplWlyr69JYGz_vTfAjA8HzzYLdXm-p5gHjqgDihY" />
         <link rel="icon" href="/favicon.ico" type="image/x-icon" />
-        <link rel='preconnect' href='https://mc.yandex.ru'/>
+        <link rel='preconnect' href='https://mc.yandex.ru' />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <meta property='og:url' content={`discocode.ru${router.asPath}`}/>
-        <meta property='og:locale' content={'ru_RU'}/>
+        <meta property='og:url' content={`discocode.ru${router.asPath}`} />
+        <meta property='og:locale' content={'ru_RU'} />
       </Head>
       <YMInitializer
-      accounts={[]}
-      options={{webvisor:false,defer:true}}
-      version = "2"
+        accounts={[]}
+        options={{ webvisor: false, defer: true }}
+        version="2"
       />
 
       <Component {...pageProps} key={router.pathname} /></>
