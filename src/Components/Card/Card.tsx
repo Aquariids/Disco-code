f248a6ac035c120c79b560ad249baaf158cf4384
Dima kun <FavoritePonyville@gmail.com>
2022-12-12 20:40:15 +0300

    test

--- src/Components/Card/Card.tsx
+++ src/Components/Card/Card.tsx
@@ -5,21 +5,30 @@ import s from './Card.module.scss';
 import Link from 'next/link';
 const { body, footer, container, body__bg, container__js, container__ts, container__next, container__react, card__title } = s;
 const Card = ({ title, color, url }: ICard) => {
+
+    function addBacgroundActiveCard(e:any) {
+        const activeEl = e.target;
+        if(activeEl && activeEl.classList.contains(container__react)) {
+            activeEl.classList.add(s.activeCardReact);
+        }
+    }
+    function removeBacgroundActiveCard(e:any) {
+        const activeEl = e.target;
+        if(activeEl && activeEl.classList.contains(container__react)) {
+            activeEl.classList.remove(s.activeCardReact);
+        }
+        
+    }
     return (
         <div className={s.card}>
             <div className={card__title}>{title} </div>
             <Link href={url}>
-           
-                <div className={cn(container, {
+                <div onMouseOver={addBacgroundActiveCard} onMouseOut={removeBacgroundActiveCard} className={cn(container, {
                     [container__js]: color === 'js',
                     [container__ts]: color === 'ts',
                     [container__next]: color === 'next',
                     [container__react]: color === 'react',
                 })} >
-                    <div className={body}>
-                        <div className={body__bg}>
-                        </div>
-                    </div>
                     
                 </div>
                 <div className={footer}>
