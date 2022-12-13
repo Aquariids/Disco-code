a97b47fbb0b9704829efe9d01f61fadc614812be
Dima kun <FavoritePonyville@gmail.com>
2022-12-13 17:06:00 +0300

    optimization scss FooterMenu

--- src/Components/FooterMenu/FooterMenu.tsx
+++ src/Components/FooterMenu/FooterMenu.tsx
@@ -1,4 +1,4 @@
-import s from './FooterMenu.module.css';
+import s from './FooterMenu.module.scss';
 import { FooterProps } from './FooterMenu.props';
 import cn from 'classnames';
 import { useRouter } from 'next/router';
@@ -6,7 +6,7 @@ import Link from 'next/link';
 import LinkA from '../../../public/link.svg';
 import { format } from 'date-fns';
 
-const { footer, content, } = s;
+const { footer, footer__content, } = s;
 
 
 const FooterMenu = ({ ...props }: FooterProps): JSX.Element => {
@@ -17,35 +17,33 @@ const FooterMenu = ({ ...props }: FooterProps): JSX.Element => {
     return (
         <>
             <div className={cn(footer, {
-                [s.footer2]: r >= 3
+                [s.footerWithoutRightSide]: r >= 3
             })} {...props}>
-                <div className={content}>
-                    <div className={s.nav}>
-                        <h3 className={s.footer_heading}> Навигация по сайту </h3>
-                        <ul className={s.footer_nav_list} style={{ listStyleType: 'none' }}>
-                            <li className={s.footer_item}><Link href={'/js/basic-js/browser-methods-and-console'}>Базовый javaScript</Link></li>
-                            <li className={s.footer_item}><Link href={'/js/advanced-js/hoisting'}>Продвинутый javaScript</Link></li>
-                            <li className={s.footer_item}><Link href={'/js/algorithms-js/binary-search'}>Алгоритмы javaScript</Link></li>
-                            <li className={s.footer_item}><Link href={'/js/practice-js/webpack'}>Практика js & Webpack</Link></li>
-                            <li className={s.footer_item}><Link href={'/js/under-the-hood-js/engine'}>JavaScript под капотом</Link></li>
-                            <li className={s.footer_item}><Link href={'/react/basic-react/react-single-page-app'}> Базовый React </Link></li>
-                            <li className={s.footer_item}><Link href={'/ts/basic-ts/installation'}>Основы typeScript</Link></li>
-                            <li className={s.footer_item}><Link href={'/next-js/basic-next-js/create-next-app'}>Начало работы с next.js</Link></li>
-                            <li className={s.footer_item}><Link href={'/paradigms/style/imperative-and-declarative'}>Стили написания кода</Link></li>
-                            <li className={s.footer_item}><Link href={'/web-request-response'}>Как работает web - request & response</Link></li>
+                <div className={footer__content}>
+                    <div className={s.footer__content__nav}>
+                        <h3 className={s.footer__heading}> Навигация по сайту </h3>
+                        <ul className={s.footer__navList} style={{ listStyleType: 'none' }}>
+                            <li className={s.footer__item}><Link href={'/js/basic-js/browser-methods-and-console'}>Базовый javaScript</Link></li>
+                            <li className={s.footer__item}><Link href={'/js/advanced-js/hoisting'}>Продвинутый javaScript</Link></li>
+                            <li className={s.footer__item}><Link href={'/js/algorithms-js/binary-search'}>Алгоритмы javaScript</Link></li>
+                            <li className={s.footer__item}><Link href={'/js/practice-js/webpack'}>Практика js & Webpack</Link></li>
+                            <li className={s.footer__item}><Link href={'/js/under-the-hood-js/engine'}>JavaScript под капотом</Link></li>
+                            <li className={s.footer__item}><Link href={'/react/basic-react/react-single-page-app'}> Базовый React </Link></li>
+                            <li className={s.footer__item}><Link href={'/ts/basic-ts/installation'}>Основы typeScript</Link></li>
+                            <li className={s.footer__item}><Link href={'/next-js/basic-next-js/create-next-app'}>Начало работы с next.js</Link></li>
+                            <li className={s.footer__item}><Link href={'/paradigms/style/imperative-and-declarative'}>Стили написания кода</Link></li>
+                            <li className={s.footer__item}><Link href={'/web-request-response'}>Как работает web - request & response</Link></li>
 
 
                         </ul>
                     </div>
 
-                    <div className={s.social}>
-                        <h3 className={s.footer_heading}> Где меня найти </h3>
-                        <ul className={s.footer_nav_list} style={{ listStyleType: 'none' }}>
-                            <li className={s.footer_item}><a href="mailto:favoritePonyville@gmail.com">FavoritePonyville@gmail.com  </a> <LinkA /></li>
-                            <li className={s.footer_item}><a target={"_blank"} href={'https://github.com/Aquariids'}>Github  </a> <LinkA /></li>
-                            <li className={s.footer_item}><a target={"_blank"} href={'https://t.me/Aquariida'}>Телеграм  </a> <LinkA /></li>
-
-
+                    <div className={s.footer__social}>
+                        <h3 className={s.footer__heading}> Где меня найти </h3>
+                        <ul className={s.footer__navList} style={{ listStyleType: 'none' }}>
+                            <li className={s.footer__item}><a href="mailto:favoritePonyville@gmail.com">FavoritePonyville@gmail.com  </a> <LinkA /></li>
+                            <li className={s.footer__item}><a target={"_blank"} href={'https://github.com/Aquariids'}>Github  </a> <LinkA /></li>
+                            <li className={s.footer__item}><a target={"_blank"} href={'https://t.me/Aquariida'}>Телеграм  </a> <LinkA /></li>
                         </ul>
                     </div>
 
@@ -53,8 +51,8 @@ const FooterMenu = ({ ...props }: FooterProps): JSX.Element => {
                 </div>
 
             </div>
-            <div className={s.copyright}>
-                <span className={s.doc}><Link href={'/docs'}>Документация по сайту</Link></span>
+            <div className={s.footer__copyright}>
+                <span className={s.footer__doc}><Link href={'/docs'}>Документация по сайту</Link></span>
                 <span >
                     {`© 2022 - ${format(new Date(), 'yyyy', )} Дмитрий Черномашенцев`}
 
