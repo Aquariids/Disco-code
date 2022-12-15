638089c3ef5662c56a5078bcf8a7eca7cdb3ffc0
Dima kun <FavoritePonyville@gmail.com>
2022-12-15 19:52:02 +0300

    fix component/News

--- src/Components/News/News.tsx
+++ src/Components/News/News.tsx
@@ -4,11 +4,11 @@ import s from './News.module.scss';
 const News = () => {
 
     const [posts,setposts] = useState([
-        {body:`Добавлен новый раздел "Практика js" и добавил статью про`, link:'webpack', src:'js/practice-js/webpack', date: '17.11.2022'},
-        {body:`Написал о том как сделать`, link:'классический слайдер на js', src:'js/practice-js/slider-js', date: '19.11.2022'},
-        {body:`Добавлена новая статья о том как работают`, link:'web запросы и ответы', src:'js/practice-js/webpack', date: '30.11.2022'},
-        {body:`Закончил писать про`, link:'fetch & promise', src:'js/advanced-js/promise-fetch_api', date: '02.12.2022'},
-        {body:`Добавлен новый раздел "JavaScript под капотом" и первая статья о работе`, link:'движка в javaScript & JRE', src:'js/under-the-hood-js/engine', date: '7.12.2022'},
+        {id: 1,body:`Добавлен новый раздел "Практика js" и добавил статью про`, link:'webpack', src:'js/practice-js/webpack', date: '17.11.2022'},
+        {id: 2,body:`Написал о том как сделать`, link:'классический слайдер на js', src:'js/practice-js/slider-js', date: '19.11.2022'},
+        {id: 3,body:`Добавлена новая статья о том как работают`, link:'web запросы и ответы', src:'js/practice-js/webpack', date: '30.11.2022'},
+        {id: 4,body:`Закончил писать про`, link:'fetch & promise', src:'js/advanced-js/promise-fetch_api', date: '02.12.2022'},
+        {id: 5,body:`Добавлен новый раздел "JavaScript под капотом" и первая статья о работе`, link:'движка в javaScript & JRE', src:'js/under-the-hood-js/engine', date: '7.12.2022'},
 
 
       ]);
@@ -20,7 +20,7 @@ const News = () => {
                 posts.map((post)=> {
                     return (
                     <>
-                        <span className={s.news__item}>{post.body} <Link href={post.src}>{post.link}</Link><span className={s.news__date}>{post.date}</span></span>
+                        <span key={post.id} className={s.news__item}>{post.body} <Link href={post.src}>{post.link}</Link><span className={s.news__date}>{post.date}</span></span>
                     </>);
                 })
             }
