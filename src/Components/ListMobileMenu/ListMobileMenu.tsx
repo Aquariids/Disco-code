5a60196add094533368f05b72d92830c9ad68861
Dima kun <FavoritePonyville@gmail.com>
2022-12-22 00:01:17 +0300

    fix

--- src/Components/ListMobileMenu/ListMobileMenu.tsx
+++ src/Components/ListMobileMenu/ListMobileMenu.tsx
@@ -7,18 +7,18 @@ const ListMobileMenu = () => {
     const router = useRouter();
     return (
         <>
-                {router.asPath.startsWith('/js/basic-js')?<MobileModalMenu page="js"/>:<></>}
-                {router.asPath.startsWith('/js/advanced-js')?<MobileModalMenu page="js"/>:<></>}
-                {router.asPath.startsWith('/js/algorithms-js')?<MobileModalMenu page="js"/>:<></>}
-                {router.asPath.startsWith('/js/practice-js')?<MobileModalMenu page="js"/>:<></>}
-                {router.asPath.startsWith('/js/under-the-hood-js')?<MobileModalMenu page="js"/>:<></>}
-                {router.asPath.startsWith('/js/interview-js')?<MobileModalMenu page="js"/>:<></>}
-                {router.asPath.startsWith('/ts/basic-ts')?<MobileModalMenu page="ts"/>:<></>}
-                {router.asPath.startsWith('/react/basic-react')?<MobileModalMenu page="react"/>:<></>} 
-                {router.asPath.startsWith('/react/interview-react')?<MobileModalMenu page="react"/>:<></>} 
-                {router.asPath.startsWith('/paradigms/style')?<MobileModalMenu page="paradigms"/>:<></>} 
-                {router.asPath.startsWith('/next-js/basic-next-js')?<MobileModalMenu page='next-js'/>:<></>} 
-                {router.asPath.startsWith('/html-css/html')?<MobileModalMenu page='html-css'/>:<></>} 
+                {router.asPath.startsWith('/js/basic-js') && <MobileModalMenu page="js"/>}
+                {router.asPath.startsWith('/js/advanced-js')&& <MobileModalMenu page="js"/>}
+                {router.asPath.startsWith('/js/algorithms-js')&&<MobileModalMenu page="js"/>}
+                {router.asPath.startsWith('/js/practice-js')&&<MobileModalMenu page="js"/>}
+                {router.asPath.startsWith('/js/under-the-hood-js')&&<MobileModalMenu page="js"/>}
+                {router.asPath.startsWith('/js/interview-js')&&<MobileModalMenu page="js"/>}
+                {router.asPath.startsWith('/ts/basic-ts')&&<MobileModalMenu page="ts"/>}
+                {router.asPath.startsWith('/react/basic-react')&&<MobileModalMenu page="react"/>} 
+                {router.asPath.startsWith('/react/interview-react')&&<MobileModalMenu page="react"/>} 
+                {router.asPath.startsWith('/paradigms/style')&&<MobileModalMenu page="paradigms"/>} 
+                {router.asPath.startsWith('/next-js/basic-next-js')&&<MobileModalMenu page='next-js'/>} 
+                {router.asPath.startsWith('/html-css/html')&&<MobileModalMenu page='html-css'/>} 
 
         </>
     );
