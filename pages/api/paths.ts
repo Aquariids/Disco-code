8dcaeae91a227ca2dbd580842a726b6c800d7409
Dima kun <FavoritePonyville@gmail.com>
2022-12-22 16:01:50 +0300

    test

--- pages/api/paths.ts
+++ pages/api/paths.ts
@@ -1,5 +1,5 @@
 import path from "path";
-export const POSTS_PATH_BASIC_JS = path.join(process.cwd(), `/Content/javascript/basic-js`);
+export const POSTS_PATH_BASIC_JS = path.resolve('Content','javascript','basic-js');
 export const POSTS_PATH_ADVANCED_JS = path.join(process.cwd(), `/Content/javascript/advanced-js`);
 export const POSTS_PATH_ALGORITHMS_JS = path.join(process.cwd(), `/Content/javascript/algorithms-js`);
 export const POSTS_PATH_PRACTICE_JS = path.join(process.cwd(), `/Content/javascript/practice-js`);
