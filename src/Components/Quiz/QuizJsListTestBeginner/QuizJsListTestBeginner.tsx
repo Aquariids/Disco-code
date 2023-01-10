4fadb088773711c255d73888afc005ff432d2fea
Dima kun <FavoritePonyville@gmail.com>
2023-01-10 03:32:10 +0300

    loader for quiz

--- src/Components/Quiz/QuizJsListTestBeginner/QuizJsListTestBeginner.tsx
+++ src/Components/Quiz/QuizJsListTestBeginner/QuizJsListTestBeginner.tsx
@@ -1,26 +1,28 @@
 import 'highlight.js/styles/default.css';
 import { useRouter } from 'next/router';
-import {useState } from 'react';
+import {useEffect, useState } from 'react';
 import QuizJs from '../QuizJs/QuizJs';
 import s from './QuizJsListTestBeginner.module.scss';
 import { quizData1, quizData2 } from './QuizJsListTestBeginner.props';
+import LoadingQuiz from './LoadingQuiz';
 const QuizJsListTestBeginner = (): JSX.Element => {
     const [percentTest1, setPercentTest1] = useState('0');
-    const [percentTest2, setPercentTest2] = useState('0');
     const router = useRouter();
     const { test } = router.query;
+    const [loading, setLoading] = useState(true);
+    useEffect(() => {
+        setTimeout(() => setLoading(false), 1000);});
 
 
     if (test === '1') {
-        return <QuizJs localKey={'test1'} percentTest={percentTest1} setPercentTest={setPercentTest1} data={quizData1} />
-    }
-    if (test === '2') {
-        return <QuizJs localKey={'test2'} percentTest={percentTest2} setPercentTest={setPercentTest2} data={quizData2} />;
-    }
-    else {
         return (
-            <div className={s.er}>Теста под номером {test} нет. УХОДИ!</div>
+            <>
+            {!loading ? (<QuizJs localKey={'test1'} percentTest={percentTest1} setPercentTest={setPercentTest1} data={quizData1} />): <LoadingQuiz/>}
+
+            </>
         );
+    } else {
+        return <>{!loading ? (<div>Теста под номером {test} нет. УХОДИ!</div>): <LoadingQuiz/> }</>;
     }
 
 
