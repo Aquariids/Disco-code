c877b16099cc5d69b52fd13ec0ac855e454298c4
Dima kun <FavoritePonyville@gmail.com>
2023-01-22 04:45:24 +0300

    fix LoadingQuiz

--- src/Components/Quiz/QuizJsListTestBeginner/QuizJsListTestBeginner.tsx
+++ src/Components/Quiz/QuizJsListTestBeginner/QuizJsListTestBeginner.tsx
@@ -3,8 +3,7 @@ import { useRouter } from 'next/router';
 import { useEffect, useState } from 'react';
 import QuizJs from '../QuizJs/QuizJs';
 import { quizData1, quizData2, quizData3, quizData4, quizData5, quizData6, quizData7, quizData8 } from './QuizJsListTestBeginner.props';
-import LoadingQuiz from './LoadingQuiz';
-import AnimationContainer from '../../AnimationContainers/AnimationContainer';
+import LoadingQuiz from '../LoadingQuiz/LoadingQuiz';
 
 const QuizJsListTestBeginner = (): JSX.Element => {
     const router = useRouter();
@@ -16,7 +15,7 @@ const QuizJsListTestBeginner = (): JSX.Element => {
     });
 
 
-    if (test === '1') return !loading ? ( <QuizJs localKey={'test1'} data={quizData1} />) : <LoadingQuiz />;
+    if (test === '1') return !loading ? (<QuizJs localKey={'test1'} data={quizData1} />) : <LoadingQuiz />;
     if (test === '2') return !loading ? (<QuizJs localKey={'test2'} data={quizData2} />) : <LoadingQuiz />;
     if (test === '3') return !loading ? (<QuizJs localKey={'test3'} data={quizData3} />) : <LoadingQuiz />;
     if (test === '4') return !loading ? (<QuizJs localKey={'test4'} data={quizData4} />) : <LoadingQuiz />;
