756c4dc5eb2108f53f080912bb7dd824fc632ed9
Dima kun <FavoritePonyville@gmail.com>
2023-03-22 20:05:33 +0300

    write about bigO/algrithms-js

--- test.js
+++ test.js
@@ -1,173 +1,241 @@
-// // //лексическое глобальное окружение
-// // // function hi
-// // // outer: null
-// // console.log(hi);
+// // // // //лексическое глобальное окружение
+// // // // // function hi
+// // // // // outer: null
+// // // // console.log(hi);
 
-// // function hi () {
-// //     // лексическое окружение функции
-// //     // hello:'hello'
-// //     // outer:global
-// //     var hello = 'hello';
-// //     console.log(hi);
+// // // // function hi () {
+// // // //     // лексическое окружение функции
+// // // //     // hello:'hello'
+// // // //     // outer:global
+// // // //     var hello = 'hello';
+// // // //     console.log(hi);
 
+// // // // }
+
+
+// // // // hi();
+
+
+
+
+
+// // // // // foo();
+// // // // console.log(foo);
+// // // // var foo = function () {
+// // // //     console.log('hi');
+// // // // }
+
+// // // // function do_something() {
+// // // //         var a = 1;
+// // // //         let b = 2;
+// // // //         while (true) {
+// // // //             var c = 3;
+// // // //             let d = 4;
+
+// // // //             break;
+// // // //         }
+// // // //         console.log(d);
+// // // //      }
+
+// // // //      do_something();
+
+
+
+// // // // Заход в новую блочную область видимости
+// // // // Начало временной мертвой зоны
+
+
+// // // // function do_something() {
+// // // //          var a = 1;
+// // // //          let b = 2;
+// // // //          while (true) {
+// // // //              var c = 3;
+// // // //              let d = 4;
+// // // //              console.log(b);
+// // // //              break;
+// // // //          }
+// // // //          console.log(d);
+// // // //       }
+
+// // // //      do_something();
+
+// // // // global EC
+// // // // let hi = 'hi';
+// // // // console.log(name); // undefined
+// // // // if (true) {
+// // // //     var name = "Dima";
+// // // //     let surname = "Lubimyi";
+// // // // }
+// // // // console.log(name); // Dima
+
+
+// // // // hi()
+
+
+// // // // let hi = function() {
+// // // //     console.log('Привет красавчик:з');
+// // // // };
+
+
+
+// // // // hi();
+
+// // // // var hi = () => {
+// // // //     console.log('Привет красавчик:з');
+// // // // };
+
+
+
+
+// // // // let c = 3;
+// // // // function a() {
+// // // //     let a = 1;
+// // // //     console.log(c); // 3
+// // // //     function b() {
+// // // //         let b = 2;
+// // // //         console.log(a); // 1
+// // // //     }
+
+// // // //     b();
+// // // // }
+
+// // // // a();\
+
+// // // // const obj2 = {
+// // // //     a:19
+// // // // }
+
+// // // // const obj = {
+// // // //     x: 10,
+// // // //     foo() {
+// // // //         setTimeout(() => {  // используем стрелочную функцию
+// // // //             console.log(this.x);  // this будет ссылаться на родительский объект obj, а не на window, как в обычной функции
+// // // //         }, 1000);
+// // // //     }
+// // // // };
+
+// // // // obj.foo(); // выведет 10 через 1 секунду
+
+
+
+
+
+// // // // const sayName = (surname) => {
+// // // //     console.log(this);
+// // // //     console.log(`${this.name} ${surname}`);
+// // // //     console.log(this.age);
+// // // // };
+
+// // // // let us = {
+// // // //     name: 'dima',
+// // // //     age: 10
+// // // // };
+
+// // // // sayName.call(us,'anime');// просто вызываем у функции метод.
+// // // // // далее передаем объект, а после аргументы для функции.
+
+
+// // // "use strict";
+// // // const obj = {
+// // //     name: "Dima",
+
+// // //     getName() {
+// // //         console.log(`hello ${this.name} `);
+// // //     }
+// // // };
+
+
+// // // obj.getName(); // hello Dima 
+
+// // // const arr = [1,2,3,4,5]
+// // // function linearSearch(value, list) {
+// // //     let found = false; // флаг, сигнализирующий о том, что значение найдено
+// // //     let position = -1; // позиция, в которой значение найдено, или -1, если нет такого значения
+// // //     let index = 0;
+// // //     // пока значение не найдено или индекс меньше длины массива
+// // //     while(!found && index < list.length)
+// // //     {
+
+
+// // //       // если значение найдено
+// // //       if(list[index] == value) {
+// // //         found = true;     // флаг = истина
+// // //         position = index; // позиция равна индексу элемента в массиве
+// // //       } else {
+// // //         index += 1;
+// // //       }
+// // //     }
+
+// // //     return position;
+// // // }
+
+
+// // // // console.log(linearSearch(8,arr)); // -1
+// // // console.log(linearSearch(3,arr)); // индекс 2
+
+
+
+// // function sum (x,y) {
+// //   return x + y;
 // // }
 
 
-// // hi();
+// // console.log(sum(10,10));
 
 
+// // function sort(arr) {
+// //   for () { // внешний цикл
+// //     let indexMin = i;
+// //     for () { // внутрений цикл, 
+// //       if (true) {
+// //         for () {  // еще внутрений цикл, 
+// //           if (true) {
 
-
-
-// // // foo();
-// // console.log(foo);
-// // var foo = function () {
-// //     console.log('hi');
-// // }
-
-// // function do_something() {
-// //         var a = 1;
-// //         let b = 2;
-// //         while (true) {
-// //             var c = 3;
-// //             let d = 4;
-
-// //             break;
+// //           }
 // //         }
-// //         console.log(d);
-// //      }
-
-// //      do_something();
-
-
-
-// // Заход в новую блочную область видимости
-// // Начало временной мертвой зоны
-
-
-// // function do_something() {
-// //          var a = 1;
-// //          let b = 2;
-// //          while (true) {
-// //              var c = 3;
-// //              let d = 4;
-// //              console.log(b);
-// //              break;
-// //          }
-// //          console.log(d);
 // //       }
-
-// //      do_something();
-
-// // global EC
-// // let hi = 'hi';
-// // console.log(name); // undefined
-// // if (true) {
-// //     var name = "Dima";
-// //     let surname = "Lubimyi";
-// // }
-// // console.log(name); // Dima
-
-
-// // hi()
-
-
-// // let hi = function() {
-// //     console.log('Привет красавчик:з');
-// // };
-
-
-
-// // hi();
-
-// // var hi = () => {
-// //     console.log('Привет красавчик:з');
-// // };
-
-
-
-
-// // let c = 3;
-// // function a() {
-// //     let a = 1;
-// //     console.log(c); // 3
-// //     function b() {
-// //         let b = 2;
-// //         console.log(a); // 1
 // //     }
 
-// //     b();
+// //   }
+
+// //   return arr;
+
+
 // // }
 
-// // a();\
-
-// // const obj2 = {
-// //     a:19
-// // }
-
-// // const obj = {
-// //     x: 10,
-// //     foo() {
-// //         setTimeout(() => {  // используем стрелочную функцию
-// //             console.log(this.x);  // this будет ссылаться на родительский объект obj, а не на window, как в обычной функции
-// //         }, 1000);
-// //     }
-// // };
-
-// // obj.foo(); // выведет 10 через 1 секунду
 
 
+// const values = [2, 27, 14, 52, 31, 96, 73, 47, 22, 6];
 
+// function QuickSort(List) {
 
+//   if (List.length <= 1) {
+//     return List;
+//   }
 
-// // const sayName = (surname) => {
-// //     console.log(this);
-// //     console.log(`${this.name} ${surname}`);
-// //     console.log(this.age);
-// // };
+//   const pivot = List[List.length - 1];
+//   const leftList = [];  // 2
+//   const rightList = []; // 27, 14, 52, 31, 96, 73, 47, 22, 6
 
-// // let us = {
-// //     name: 'dima',
-// //     age: 10
-// // };
+//   for (let i = 0; i < List.length - 1; i++) {
 
-// // sayName.call(us,'anime');// просто вызываем у функции метод.
-// // // далее передаем объект, а после аргументы для функции.
+//     if (List[i] < pivot) {
+//       leftList.push(List[i]);
 
-
-// "use strict";
-// const obj = {
-//     name: "Dima",
-
-//     getName() {
-//         console.log(`hello ${this.name} `);
 //     }
-// };
+//     else {
+//       rightList.push(List[i]);
+//     }
+//   }
+
+//   return [...QuickSort(leftList), pivot, ...QuickSort(rightList)];
+// }
+
+// console.log(QuickSort(values));
 
 
-// obj.getName(); // hello Dima 
-
-const arr = [1,2,3,4,5]
-function linearSearch(value, list) {
-    let found = false; // флаг, сигнализирующий о том, что значение найдено
-    let position = -1; // позиция, в которой значение найдено, или -1, если нет такого значения
-    let index = 0;
- 
-    // пока значение не найдено или индекс меньше длины массива
-    while(!found && index < list.length)
-    {
-      // если значение найдено
-      if(list[index] == value) {
-        found = true;     // флаг = истина
-        position = index; // позиция равна индексу элемента в массиве
-      } else {
-        index += 1;
-      }
-    }
-
-    return position;
-}
 
 
-console.log(linearSearch(8,arr)); // -1
-console.log(linearSearch(3,arr)); // индекс 2
+let arr = [1,2,3,4,5];
+
+console.log(arr[2]) // 3
